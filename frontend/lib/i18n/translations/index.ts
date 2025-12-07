17b2607f57633339b1aee9cab622f1a2f0604607
Márton Horváth <hmartonsandor@gmail.com>
2025-12-07 10:47:17 +0100

    add mexican spanish translations

--- frontend/lib/i18n/translations/index.ts
+++ frontend/lib/i18n/translations/index.ts
@@ -1,10 +1,12 @@
 import { CountryCode, Translations } from '@/types/i18n';
 import { en } from './en';
 import { hu } from './hu';
+import { mx } from './mx';
 
 export const translations: Record<CountryCode, Translations> = {
   US: en,
   HU: hu,
+  MX: mx,
 };
 
-export { en, hu };
+export { en, hu, mx };
